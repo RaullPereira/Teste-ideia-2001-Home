7f18afc01511d81175cce9438f0ea39a600a3f8f
Raul <110264497+Raulpss@users.noreply.github.com>
2022-10-18 05:59:51 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,27 @@
+
+var button = document.querySelector(".btn-contato");
+
+button.onclick = function() {
+    window.location.href = "https://tela-redefinir-senha.vercel.app/";
+}
+
+//SITE//
+var button = document.querySelector(".img-site");
+
+button.onclick = function() {
+    window.location.href = "https://ideia2001.com.br";
+}
+
+//FACEBOO//
+var button = document.querySelector(".img-face");
+
+button.onclick = function() {
+    window.location.href = "https://www.facebook.com/Ideia2001"
+}
+
+//INSTAGRAM//
+var button = document.querySelector(".img-insta");
+
+button.onclick = function() {
+    window.location.href = "https://www.instagram.com/ideia2001"
+}
